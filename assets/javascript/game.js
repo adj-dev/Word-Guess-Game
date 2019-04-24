3592955bd32d536d0e363bcfa78c47d6bb2bfa96
adj-dev <andrew.johnson2073@gmail.com>
2019-04-24 13:14:14 -0500

    Modified the structure of the top game object

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,41 +1,34 @@
 const game = {
-  startGame: function() {},
-  wins: 0,
-  tries: 10,
-  words: ['pasta', 'spaghetti', 'linguini'],
-  randomWord: function() {
-    let n = Math.floor(Math.random() * this.words.length);
-    let word = this.words[n];
-    // this.convertStringToArray(word);
-    this.convertStringToArray.chosenWord = word;
+  start: function() {
+    this.library.getRandomWord();
   },
-  // chosenWord: [],
-  // convertStringToArray: function(word) {
-  //   console.log(word);
-  //   for (let i = 0; i < word.length; i++) {
-  //     chosenWord.push(word.charAt(i));
-  //   }
-  // },
-  convertStringToArray: {
-    chosenWord: String,
-    wordArray: [],
-    convert: function(word) {
-      for (let i = 0; i < word.length; i++) {
-        this.wordArray.push(word.charAt(i));
-      }
+  score: {
+    current: 0,
+    tries: 10,
+    win: function() {
+      this.current += 1;
+    },
+    wrongGuess: function() {
+      this.tries -= 1;
+    },
+    reset: function() {
+      this.tries = 10;
     }
   },
-  incorrectLetters: [],
-  winGame: function() {
-    this.wins += 1;
-    renderWins();
-  },
-  incorrectGuess: function() {
-    this.tries -= 1;
-  },
-  reset: function() {
-    this.wins = 0;
-    this.tries = 10;
+  library: {
+    index: ['beethoven', 'liszt', 'mozart', 'chopin'],
+    currentWord: [],
+    getRandomWord: function() {
+      let i = Math.floor(Math.random() * this.index.length);
+      let word = this.index[i];
+      this.setRandomWord(word);
+    },
+    setRandomWord: function(word) {
+      this.currentWord = [];
+      for (let i = 0; i < word.length; i++) {
+        this.currentWord.push(word.charAt(i));
+      }
+    }
   }
 };
 
